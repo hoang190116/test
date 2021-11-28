eed7d79305bda577a0b90c12ee5388da1526db86
hoang190116 <83691515+hoang190116@users.noreply.github.com>
2021-11-28 22:53:27 +0700

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -38,11 +38,11 @@ import java.util.Map;
 // @SpringBootApplication
 public class Main {
 
-//  @Value("${spring.datasource.url}")
+ @Value("${spring.datasource.url}")
   private String dbUrl;
 
-//   @Autowired
-//   private DataSource dataSource;
+  @Autowired
+  private DataSource dataSource;
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
