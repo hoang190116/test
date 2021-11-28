1bc8c727565fb3e9761e74d691b412d1256d333d
hoang190116 <83691515+hoang190116@users.noreply.github.com>
2021-11-28 22:38:28 +0700

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -21,7 +21,7 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +35,7 @@ import java.util.ArrayList;
 import java.util.Map;
 
 @Controller
-//@SpringBootApplication
+@SpringBootApplication
 public class Main {
 
 //  @Value("${spring.datasource.url}")
